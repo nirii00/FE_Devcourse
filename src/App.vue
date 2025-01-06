548fdccca68662687aa1524e5df246001c1e3522
nirii00 <nirii00@users.noreply.github.com>
2025-01-06 11:45:46 +0900

    20250106_login

---
+++ src/App.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="flex items-center justify-center min-h-screen bg-gray-900">
+    <div
+      class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700"
+    >
+      <form class="space-y-6" @submit="($event) => handleSubmit($event)">
+        <h5 class="text-xl font-medium text-gray-900 dark:text-white">
+          Sign in to our platform
+        </h5>
+        <div>
+          <label
+            for="email"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >Your email</label
+          >
+          <input
+            type="email"
+            name="email"
+            id="email"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            placeholder="name@company.com"
+            autocomplete="off"
+            required
+            v-model="email"
+          />
+        </div>
+        <div>
+          <label
+            for="password"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >Your password</label
+          >
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="••••••••"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            required
+            v-model="password"
+          />
+        </div>
+        <div class="flex items-start">
+          <div class="flex items-start">
+            <div class="flex items-center h-5">
+              <input
+                id="remember"
+                type="checkbox"
+                value=""
+                class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                v-model="isChecked"
+              />
+            </div>
+            <label
+              for="remember"
+              class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >Remember me</label
+            >
+          </div>
+          <a
+            href="#"
+            class="ms-auto text-sm text-blue-700 hover:underline dark:text-blue-500"
+            >Lost Password?</a
+          >
+        </div>
+        <button
+          type="submit"
+          class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:bg-gray-300 disabled:cursor-not-allowed"
+          :disabled="!(email && password)"
+        >
+          Login to your account
+        </button>
+        <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
+          Not registered?
+          <a href="#" class="text-blue-700 hover:underline dark:text-blue-500"
+            >Create account</a
+          >
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      email: "",
+      password: "",
+      isChecked: false,
+    };
+  },
+  methods: {
+    handleSubmit($event) {
+      $event.preventDefault();
+      alert(
+        `Login Success!\nEmail: ${this.email}\nPassword: ${this.password}\nRemember Me: ${this.isChecked}`
+      );
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
